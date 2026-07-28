ab48c550ac4033d3678fe78e56100158da692282
Vladimir5863 <vlada.nikolic2002@gmail.com>
2026-07-28 21:00:12 +0200

    Admin router and controller finished

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,6 +10,7 @@ import meRouter from "./routes/meRouter";
 import productRouter from "./routes/productRouter";
 import streamRouter from "./routes/streamRouter";
 import checkoutRouter from "./routes/checkoutRouter";
+import adminRouter from "./routes/adminRouter";
 import * as Sentry from "@sentry/node";
 
 import fs from "node:fs";
@@ -42,6 +43,7 @@ app.use("/api/me", meRouter);
 app.use("/api/products", productRouter);
 app.use("/api/stream", streamRouter);
 app.use("/api/checkout", checkoutRouter);
+app.use("/api/admin", adminRouter);
 
 const publicDir = path.join(process.cwd(), "public");
 if (fs.existsSync(publicDir)) {
